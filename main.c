96997d34c2a505d0471455d2166e0dfd3a91ab29
H. Lucas <151595379+lucasln10@users.noreply.github.com>
2025-04-17 00:17:29 +0000

    new

---
+++ main.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+	setlocale(LC_ALL, "Portuguese");
+	
+	float temp;
+	char u;
+	
+	while (1){
+		printf("digite a temperatura e a unidade: ");
+		scanf("%f %c", &temp, &u);
+		printf("Digite a unidade para a qual quer a conversão: ");
+		scanf(" %c", &u);
+		
+		if (u1 == "K" || u1 == "k"){
+			float result = (temp + 273.15);
+			printf("A conversão de Fahrenheit para Celcius é %.2f F.", result);
+			printf("\n\n");
+		}else if (u == "F" || u == "f"){
+			float result = (temp - 32) * 5/9;
+			printf("A conversão de Fahrenheit para Celcius é %.2f F.", result);
+			printf("\n\n");
+			
+		}else if (u == "C" || u == "c"){
+			float result = (temp * 1.8) + 32;
+			printf("A conversão de Celcius para Fahrenheit é %.2f C.", result);
+			printf("\n\n");
+			
+		}
+	}
+	return 0;
+}
