bd6751a21619e92baaf377ce78e2a772308f9d0c
H. Lucas <151595379+lucasln10@users.noreply.github.com>
2025-04-17 00:35:06 +0000

    nd

--- main.c
+++ main.c
@@ -5,7 +5,7 @@
 int main() {
 	setlocale(LC_ALL, "Portuguese");
 	
-	float temp;
+	float temp, resultado;
 	char u;
 	
 	while (1){
@@ -14,21 +14,33 @@ int main() {
 		printf("Digite a unidade para a qual quer a conversão: ");
 		scanf(" %c", &u);
 		
-		if (u1 == "K" || u1 == "k"){
-			float result = (temp + 273.15);
-			printf("A conversão de Fahrenheit para Celcius é %.2f F.", result);
+		printf("\n");
+		
+		if (u == "K" || u == "k"){
+			resultado = (temp + 273.15);
+			printf("A conversão de Fahrenheit para Celcius é %.2f F.", resultado);
 			printf("\n\n");
 		}else if (u == "F" || u == "f"){
-			float result = (temp - 32) * 5/9;
-			printf("A conversão de Fahrenheit para Celcius é %.2f F.", result);
+			resultado = (temp - 32) * 5/9;
+			printf("A conversão de Fahrenheit para Celcius é %.2f F.", resultado);
 			printf("\n\n");
 			
 		}else if (u == "C" || u == "c"){
-			float result = (temp * 1.8) + 32;
-			printf("A conversão de Celcius para Fahrenheit é %.2f C.", result);
+			resultado = (temp * 1.8) + 32;
+			printf("A conversão de Celcius para Fahrenheit é %.2f C.", resultado);
 			printf("\n\n");
 			
+		}else {
+			break;
 		}
+		
+		
+		if (resultado > 37.78){
+			printf("Você está com febre.\n");
+		}else {
+			printf("Você não está com febre.\n");
+		}
+		
 	}
 	return 0;
 }
